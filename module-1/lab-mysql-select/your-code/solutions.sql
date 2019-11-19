7e33aa041342b62fe564af6173459eb6572c2082
BORJA <borjauria@MacBook-Pro-de-BORJA.local>
2019-11-19 18:34:28 +0100

    Challenge 1 done, Challenge 2 done, there's light at the end of the tunnel

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -34,3 +34,19 @@ JOIN publishers
 ON titles.pub_id = publishers.pub_id
 GROUP BY titleauthor.au_id
 ORDER BY authors.au_id DESC;
+
+Challenge 3 - Best Selling Authors
+
+SELECT
+authors.au_id as 'AUTHOR ID',
+authors.au_lname as 'LAST NAME',
+authors.au_fname as 'FIRST NAME'
+FROM authors
+JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+JOIN publishers
+ON titles.pub_id = publishers.pub_id
+
+
